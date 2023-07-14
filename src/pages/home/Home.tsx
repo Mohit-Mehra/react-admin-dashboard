475394930a7a835f0d4eb40d43460fd092789d64
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-07-14 11:03:25 +0530

    box 8 and 9 complete

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,11 +2,14 @@ import ChartBox from "../../components/chartBox/ChartBox";
 import TopBox from "../../components/topBox/TopBox";
 import "./home.scss";
 import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
   chartBoxConversion,
   chartBoxProduct,
   chartBoxRevenue,
   chartBoxUser,
 } from "../../data.ts";
+import BarChartBox from "../../components/barChartBox/BarChartBox.tsx";
 
 const Home = () => {
   return (
@@ -28,8 +31,12 @@ const Home = () => {
         <ChartBox {...chartBoxRevenue} />
       </div>
       <div className="box box7">Box7</div>
-      <div className="box box8">Box8</div>
-      <div className="box box9">Box9</div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+      <BarChartBox {...barChartBoxRevenue} />
+      </div>
     </div>
   );
 };
